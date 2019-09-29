b75783650b885216eb9087f7aea562803df36f86
Garrett Boulais <gboulais@Ucsd.edu>
2019-09-29 11:26:55 -0700

    Trying to fix login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,27 +14,25 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // cookie can not be modified, note we cant delete it now!! :(
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  };
   // only sends over https
   // if (req.secure) cookieOptions.secure = true;
   // second part relates to heroku
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
 
-  res.cookie('jwt', token, cookieOptions);
-
-  // hide password from output
+  // Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
-    token: token,
+    token,
     data: {
-      user: user
+      user
     }
   });
 };
